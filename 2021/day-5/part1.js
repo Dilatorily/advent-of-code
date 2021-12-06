05537930766245c026e67f59249084c2c0f08222
Huy Dang Lê-Ngô <huy-dang.le-ngo@polymtl.ca>
2021-12-05 22:35:54 -0800

    Add 2021-12-05

---
+++ 2021/day-5/part1.js
@@ -0,0 +1,28 @@
+console.log(
+  Object.entries(
+    document
+      .querySelector('pre')
+      .textContent.split('\n')
+      .slice(0, -1)
+      .map((vents) => {
+        const vector = vents.split(' -> ');
+        return vector.map((coordinates) =>
+          coordinates.split(',').map((coordinate) => parseInt(coordinate, 10)),
+        );
+      })
+      .filter((vector) => vector[0][0] === vector[1][0] || vector[0][1] === vector[1][1])
+      .reduce((grid, vector) => {
+        const isVertical = vector[0][0] === vector[1][0];
+        const index = isVertical ? 1 : 0;
+        const start = Math.min(vector[0][index], vector[1][index]);
+        const end = Math.max(vector[0][index], vector[1][index]);
+
+        for (let i = start; i <= end; i += 1) {
+          const coordinates = `${isVertical ? vector[0][0] : i},${isVertical ? i : vector[0][1]}`;
+          Object.assign(grid, { [coordinates]: (grid[coordinates] || 0) + 1 });
+        }
+
+        return grid;
+      }, {}),
+  ).filter(([, overlap]) => overlap >= 2).length,
+);
