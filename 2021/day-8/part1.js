ec9d3a0ba71e48b69ce91b8412f6926aa12c28c8
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2021-12-08 21:06:12 -0800

    Add 2021-12-08

---
+++ 2021/day-8/part1.js
@@ -0,0 +1,38 @@
+document
+  .querySelector('pre')
+  .textContent.split('\n')
+  .slice(0, -1)
+  .map((string) => {
+    const [patterns, output] = string.split(' | ');
+    return { output: output.split(' '), patterns: patterns.split(' ') };
+  })
+  .reduce((sum, { output, patterns }) => {
+    const digits = {};
+    let count = 0;
+
+    patterns.forEach((pattern) => {
+      if (pattern.length === 2) {
+        digits[1] = pattern.split('').sort().join('');
+      }
+
+      if (pattern.length === 4) {
+        digits[4] = pattern.split('').sort().join('');
+      }
+
+      if (pattern.length === 3) {
+        digits[7] = pattern.split('').sort().join('');
+      }
+
+      if (pattern.length === 7) {
+        digits[8] = pattern.split('').sort().join('');
+      }
+    });
+
+    output.forEach((digit) => {
+      if (Object.values(digits).includes(digit.split('').sort().join(''))) {
+        count += 1;
+      }
+    });
+
+    return sum + count;
+  }, 0);
