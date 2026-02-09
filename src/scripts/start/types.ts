7b3646477ec2500787dffa0048b0a0a4e2b6e130
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2026-02-09 15:57:35 -0500

    Simplify types

--- src/scripts/start/types.ts
+++ src/scripts/start/types.ts
@@ -8,23 +8,11 @@ export interface ScaffoldPaths {
   test2File: string;
 }
 
-interface Part1TestCase {
+export interface TestCase {
   input: string;
-  part1Output: number | string;
+  part1Output?: number | string;
+  part2Output?: number | string;
 }
-
-interface Part2TestCase {
-  input: string;
-  part2Output: number | string;
-}
-
-interface BothPartTestCase {
-  input: string;
-  part1Output: number | string;
-  part2Output: number | string;
-}
-
-export type TestCase = Part1TestCase | Part2TestCase | BothPartTestCase;
 
 export interface SolutionModule<T = number | string> {
   solution?: (lines: string[]) => T;
