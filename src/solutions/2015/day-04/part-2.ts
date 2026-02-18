b94abd7b345892326402c8fe110bc41f2cf130e1
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2026-02-18 09:38:22 -0500

    Add 2015-12-04

---
+++ src/solutions/2015/day-04/part-2.ts
@@ -0,0 +1,16 @@
+import crypto from 'node:crypto';
+
+export const solution = (lines: string[]) => {
+  const key = lines[0];
+
+  let index = 1;
+  while (index < Number.MAX_SAFE_INTEGER) {
+    if (crypto.createHash('md5').update(`${key}${index}`).digest('hex').startsWith('000000')) {
+      break;
+    }
+
+    index += 1;
+  }
+
+  return index;
+};
---
+++ src/solutions/2015/day-04/test-cases.ts
@@ -0,0 +1,7 @@
+import type { TestCase } from '#dilatorily/advent-of-code/scripts/start/types';
+
+export const testCases: TestCase[] = [
+  { input: 'abcdef', part1Output: 609043 },
+  { input: 'pqrstuv', part1Output: 1048970 },
+  { input: 'a', part2Output: 14779946 },
+];
