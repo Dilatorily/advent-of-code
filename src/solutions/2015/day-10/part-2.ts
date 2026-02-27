e4cf420b7c4ef0c18de449d7c400b16d49c9ae07
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2026-02-27 10:26:28 -0500

    Add 2015-12-10

---
+++ src/solutions/2015/day-10/part-2.ts
@@ -0,0 +1,35 @@
+import { stringToCharArray } from '#dilatorily/advent-of-code/utility/string-to-char-array';
+
+export const solution = (lines: string[], iterations = 50) => {
+  const lookAndSay = (sequence: string, iterationsLeft: number) => {
+    if (iterationsLeft === 0) {
+      return sequence.length;
+    }
+
+    const numbers = stringToCharArray(sequence);
+    let newSequence = '';
+    let currentNumber = numbers[0];
+    let count = 1;
+
+    for (let index = 0; index < numbers.length; ++index) {
+      if (index === 0) {
+        continue;
+      }
+
+      const number = numbers[index];
+
+      if (number === currentNumber) {
+        count += 1;
+      } else {
+        newSequence += `${count}${currentNumber}`;
+        currentNumber = number;
+        count = 1;
+      }
+    }
+    newSequence += `${count}${currentNumber}`;
+
+    return lookAndSay(newSequence, iterationsLeft - 1);
+  };
+
+  return lookAndSay(lines[0], iterations);
+};
---
+++ src/solutions/2015/day-10/test-cases.ts
@@ -0,0 +1,9 @@
+import type { TestCase } from '#dilatorily/advent-of-code/scripts/start/types';
+
+export const testCases: TestCase[] = [
+  { input: '1', part1Output: 2, part2Output: 2 },
+  { input: '11', part1Output: 2, part2Output: 2 },
+  { input: '21', part1Output: 4, part2Output: 4 },
+  { input: '1211', part1Output: 6, part2Output: 6 },
+  { input: '111221', part1Output: 6, part2Output: 6 },
+];
