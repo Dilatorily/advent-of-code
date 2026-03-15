b6a872e079708559c94e580599c8a034318eeb2d
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2026-03-15 12:07:24 -0400

    Add 2015-12-14

---
+++ src/solutions/2015/day-14/part-1.ts
@@ -0,0 +1,51 @@
+import { toNumber } from '#dilatorily/advent-of-code/utility/to-number';
+
+const RACE_DURATION = 2503;
+
+export const solution = (lines: string[], raceDuration = RACE_DURATION) => {
+  const reindeers = lines.map((line) => {
+    const [name, sentence] = line.split(' can fly ');
+    const [speed, flySentence] = sentence.split(' km/s for ');
+    const [fly, restSentence] = flySentence.split(' seconds, but then must rest for ');
+    const [rest] = restSentence.split(' seconds.');
+
+    return {
+      fly: toNumber(fly),
+      name,
+      rest: toNumber(rest),
+      speed: toNumber(speed),
+    };
+  });
+
+  const race: Record<
+    string,
+    { distance: number; remainingFlyTime: number; remainingRestTime: number }
+  > = reindeers.reduce(
+    (accumulator, { fly, name }) =>
+      Object.assign(accumulator, {
+        [name]: { distance: 0, remainingFlyTime: fly, remainingRestTime: 0 },
+      }),
+    {},
+  );
+
+  for (let seconds = 0; seconds < raceDuration; ++seconds) {
+    reindeers.forEach(({ fly, name, rest, speed }) => {
+      if (race[name].remainingRestTime > 0) {
+        race[name].remainingRestTime -= 1;
+
+        if (race[name].remainingRestTime === 0) {
+          race[name].remainingFlyTime = fly;
+        }
+      } else if (race[name].remainingFlyTime > 0) {
+        race[name].remainingFlyTime -= 1;
+        race[name].distance += speed;
+
+        if (race[name].remainingFlyTime === 0) {
+          race[name].remainingRestTime = rest;
+        }
+      }
+    });
+  }
+
+  return Math.max(...Object.values(race).map(({ distance }) => distance));
+};
---
+++ src/solutions/2015/day-14/test-cases.ts
@@ -0,0 +1,10 @@
+import type { TestCase } from '#dilatorily/advent-of-code/scripts/start/types';
+
+export const testCases: TestCase[] = [
+  {
+    input: `Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`,
+    part1Output: 1120,
+    part2Output: 689,
+  },
+];
