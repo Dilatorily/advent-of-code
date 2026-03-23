bc5ad90a24ad0fd5085b9fd314660c82420ca65f
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2026-03-22 21:58:06 -0400

    Add 2015-12-15

---
+++ src/solutions/2015/day-15/part-1.ts
@@ -0,0 +1,79 @@
+import { toNumber } from '#dilatorily/advent-of-code/utility/to-number';
+
+interface Ingredient {
+  capacity: number;
+  durability: number;
+  flavor: number;
+  texture: number;
+}
+
+const TOTAL_TEASPOONS = 100;
+
+const generatePermutations = (teaspoons: number, totalIngredients: number) => {
+  if (totalIngredients === 1) {
+    return [[teaspoons]];
+  }
+
+  const permutations: number[][] = [];
+  for (let index = 0; index <= teaspoons; ++index) {
+    for (const permutation of generatePermutations(teaspoons - index, totalIngredients - 1)) {
+      permutations.push([index, ...permutation]);
+    }
+  }
+
+  return permutations;
+};
+
+export const solution = (lines: string[]) => {
+  const ingredients: Record<string, Ingredient> = lines.reduce((ingredients, line) => {
+    const [ingredient, properties] = line.split(': capacity ');
+    const [capacity, durabilityString] = properties.split(', durability ');
+    const [durability, flavorString] = durabilityString.split(', flavor ');
+    const [flavor, textureString] = flavorString.split(', texture ');
+    const [texture] = textureString.split(', calories ');
+    return Object.assign(ingredients, {
+      [ingredient]: {
+        capacity: toNumber(capacity),
+        durability: toNumber(durability),
+        flavor: toNumber(flavor),
+        texture: toNumber(texture),
+      },
+    });
+  }, {});
+
+  let maximumScore = Number.MIN_SAFE_INTEGER;
+  generatePermutations(TOTAL_TEASPOONS, Object.keys(ingredients).length).forEach((permutation) => {
+    const capacity = Math.max(
+      permutation.reduce((capacity, teaspoons, ingredientIndex) => {
+        const ingredient = Object.keys(ingredients)[ingredientIndex];
+        return capacity + ingredients[ingredient].capacity * teaspoons;
+      }, 0),
+      0,
+    );
+    const durability = Math.max(
+      permutation.reduce((durability, teaspoons, ingredientIndex) => {
+        const ingredient = Object.keys(ingredients)[ingredientIndex];
+        return durability + ingredients[ingredient].durability * teaspoons;
+      }, 0),
+      0,
+    );
+    const flavor = Math.max(
+      permutation.reduce((flavor, teaspoons, ingredientIndex) => {
+        const ingredient = Object.keys(ingredients)[ingredientIndex];
+        return flavor + ingredients[ingredient].flavor * teaspoons;
+      }, 0),
+      0,
+    );
+    const texture = Math.max(
+      permutation.reduce((texture, teaspoons, ingredientIndex) => {
+        const ingredient = Object.keys(ingredients)[ingredientIndex];
+        return texture + ingredients[ingredient].texture * teaspoons;
+      }, 0),
+      0,
+    );
+    const score = capacity * durability * flavor * texture;
+    maximumScore = Math.max(score, maximumScore);
+  });
+
+  return maximumScore;
+};
---
+++ src/solutions/2015/day-15/test-cases.ts
@@ -0,0 +1,10 @@
+import type { TestCase } from '#dilatorily/advent-of-code/scripts/start/types';
+
+export const testCases: TestCase[] = [
+  {
+    input: `Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`,
+    part1Output: 62842880,
+    part2Output: 57600000,
+  },
+];
